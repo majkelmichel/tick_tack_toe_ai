0ba6cf5923f77c3cef9dbc6ce3ea5df60b59ff0e
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-07-28 19:01:25 +0200

    Stage 5 Complete

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,14 @@
 from random import randint
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ai_sign = "X"
+en_sign = "O"
 
 
 class Player:
-    def __init__(self, player_type):
+    def __init__(self, player_type, player_sign):
         self.player_type: str = player_type
+        self.player_sign: str = player_sign
 
     def move(self, cells):
         if self.player_type == "user":
@@ -53,6 +56,13 @@ class Player:
 
             choice_l = [randint(0, 2), randint(0, 2)]
             return choice_l
+        elif self.player_type == "hard":
+            global ai_sign
+            global en_sign
+            ai_sign = self.player_sign
+            en_sign = "X" if ai_sign == "O" else "O"
+            cells = defactor_list(cells)
+            return get_two_dim_index(minimax(cells, self.player_sign)["index"])
 
 
 def main():
@@ -65,8 +75,8 @@ def main():
             continue
         else:
             break
-    player_x = Player(starting_args[1])
-    player_y = Player(starting_args[2])
+    player_x = Player(starting_args[1], "X")
+    player_y = Player(starting_args[2], "Y")
     play_field_cells = refactor_list([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     moves = 0
     next_to_move = "X"
@@ -78,26 +88,6 @@ def main():
             print(check_win(play_field_cells))
             return None
     print("Draw")
-
-
-# def game_start():
-#     moves_made = 0
-#     starting_cells = list(input("Enter cells: "))
-#     # print(who_first(starting_cells))
-#     if who_first(starting_cells):
-#         starting = "X"
-#     else:
-#         starting = "O"
-#
-#     starting_cells = swap_underscore_to_space(starting_cells)
-#     for cell in starting_cells:
-#         if cell == "X" or cell == "O":
-#             moves_made += 1
-#
-#     starting_cells = refactor_list(starting_cells)
-#     show_play_field(starting_cells)
-#
-#     return starting_cells, starting, moves_made
 
 
 def play_game(cells, move, player_one, player_two):
@@ -184,21 +174,6 @@ def show_play_field(cells):
     print("---------")
 
 
-# def who_first(cells):
-#     counter = 0
-#     for i in cells:
-#         if i == '_':
-#             counter += 1
-#     return counter % 2 != 0
-
-
-# def swap_underscore_to_space(list_with_cells):
-#     for i in range(len(list_with_cells)):
-#         if list_with_cells[i] == '_':
-#             list_with_cells[i] = ' '
-#     return list_with_cells
-
-
 def refactor_list(list_to_refactor):
     new = []
     i = 0
@@ -213,4 +188,88 @@ def refactor_list(list_to_refactor):
     return new
 
 
+def defactor_list(list_to_defactor):
+    new = []
+    c = 0
+    for i in list_to_defactor:
+        for j in i:
+            if j == ' ':
+                j = c
+            new.append(j)
+            c += 1
+    return new
+
+
+def check_if_win(cells, player):
+    # Horizontal check
+    if cells[0] == cells[1] == cells[2] == player or cells[3] == cells[4] == cells[5] == player\
+            or cells[6] == cells[7] == cells[8] == player or cells[0] == cells[3] == cells[6] == player\
+            or cells[1] == cells[4] == cells[7] == player or cells[2] == cells[5] == cells[8] == player\
+            or cells[0] == cells[4] == cells[8] == player or cells[2] == cells[4] == cells[6] == player:
+        return True
+    return False
+
+
+def available_spots(old_cells):
+    available_cells = []
+    for i in range(len(old_cells)):
+        if isinstance(old_cells[i], int):
+            available_cells.append(i)
+    return available_cells
+
+
+def minimax(new_cells, player):
+    possible_spots = available_spots(new_cells)
+    if check_if_win(new_cells, en_sign):
+        return {"score": -10}
+    elif check_if_win(new_cells, ai_sign):
+        return {"score": 10}
+    elif len(possible_spots) == 0:
+        return {"score": 0}
+
+    moves = []
+    for i in range(len(possible_spots)):
+        move: dict = dict()
+        # print(new_cells, new_cells[possible_spots[i]], possible_spots[i], i)
+        move["index"] = new_cells[possible_spots[i]]
+        new_cells[possible_spots[i]] = player
+        if player == ai_sign:
+            result = minimax(new_cells, en_sign)
+            move["score"] = result["score"]
+        else:
+            result = minimax(new_cells, ai_sign)
+            move["score"] = result["score"]
+
+        new_cells[possible_spots[i]] = move["index"]
+        moves.append(move)
+    # print(moves)
+    best_move = []
+    if player == ai_sign:
+        best_score = -100000
+        for i in range(len(moves)):
+            if moves[i]["score"] > best_score:
+                best_score = moves[i]["score"]
+                best_move = i
+    else:
+        best_score = 100000
+        for i in range(len(moves)):
+            if moves[i]["score"] < best_score:
+                best_score = moves[i]["score"]
+                best_move = i
+    return moves[best_move]
+
+
+def get_two_dim_index(index):
+    c = 0
+    for x in range(3):
+        for j in range(3):
+            if index == c:
+                return [x, j]
+            c += 1
+
+
 main()
+# lst = defactor_list(["X", "O", " ", "O", " ", " ", " ", "X", "X"])
+# lst = ["O",1 ,"X","X",4 ,"X", 6 ,"O","O"]
+# print(available_spots(lst))
+# print(minimax(lst, "X"))
